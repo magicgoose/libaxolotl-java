b749608f66a12f5cca15df1e31a4b546da57e3fe
Sarge <sausagefactory0@gmail.com>
2015-09-15 13:12:36 +0300

    single script to build the C app with one command

--- jni/premake4.lua
+++ jni/premake4.lua
@@ -1,15 +1,11 @@
 #!lua
 solution "libaxolotl"
-    configurations { "Debug", "Release" }
+    configurations { "Release" }
     project "libaxolotl-cli"
         kind "ConsoleApp"
         language "C"
         files { "**.h", "**.c" }
         excludes { "curve25519-jni.c"}
-        configuration "Debug"
-            defines { "DEBUG" }
-            flags { "Symbols" }
-
         configuration "Release"
             defines { "NDEBUG" }
             flags { "Optimize" }
